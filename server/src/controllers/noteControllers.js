d073f6431e541b02ef0cfa8decabfb845a247af1
Harshit Saxena <harshit214saxena@gmail.com>
2026-01-29 00:15:22 +0530

    feat: implement note management functionality with CRUD operations and JWT verification middleware

---
+++ server/src/controllers/noteControllers.js
@@ -0,0 +1,101 @@
+import mongoose from "mongoose";
+import { Note } from "../models/noteModels.js";
+import { Project } from "../models/projectModels.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+
+const getNotes = async (req, res) => {
+  const { projectId } = req.params;
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const notes = await Note.find({
+    project: new mongoose.Types.ObjectId(projectId),
+  }).populate("createdBy", " username fullname avatar");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, notes, "Notes fetched Successfully"));
+};
+
+const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findById(noteId).populate(
+    "createdBy",
+    " username fullname avatar",
+  );
+
+  if (!note) {
+    throw new ApiError(404, "Note not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, note, "Note fetched Successfully"));
+};
+
+const createNote = async (req, res) => {
+  const { projectId } = req.params;
+  const { content } = req.body || {};
+
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project Not Found");
+  }
+
+  const note = await Note.create({
+    project: new mongoose.Types.ObjectId(projectId),
+    content,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const populatedNote = await Note.findById(note._id).populate(
+    "createdBy",
+    "username fullname avatar",
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, populatedNote, "Note Created Successfully"));
+};
+
+const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const { content } = req.body;
+
+  const existingNote = Note.findById(noteId);
+  if (!existingNote) {
+    throw new ApiError(404, "Note not found");
+  }
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { content },
+    { new: true },
+  ).populate("createdBy", "username fullname avatar");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedNote, "Note Updated Successfully"));
+};
+
+const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const existingNote = Note.findById(noteId);
+
+  if (!existingNote) {
+    throw new ApiError(404, "Note not found");
+  }
+  const deletedNote = await Note.findByIdAndDelete(noteId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedNote, "Note deleted Successfully"));
+};
+
+export { createNote, getNotes, getNoteById, deleteNote, updateNote };
---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,52 @@
+import { ProjectMember } from "../models/projectmemberModels.js";
+import { User } from "../models/userModels.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorised Request");
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid Access token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
+
+export const validateProjectPermission = (roles = []) =>
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(401, "Invalid projectId");
+    }
+
+    const projectMember = await ProjectMember.findOne({
+      project: mongoose.Types.ObjectId(projectId),
+      user: mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!projectMember) {
+      throw new ApiError(401, "Project not found");
+    }
+
+    const givenRole = projectMember?.role;
+
+    req.user.role = givenRole;
+    next()
+  });
